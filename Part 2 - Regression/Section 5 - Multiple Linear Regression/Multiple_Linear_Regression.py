5b42795fd8cd1f6853238b1fb26f3826c2d56804
BOB <schlagadam@gmail.com>
2020-03-21 10:22:32 -0500

    Created Multi_Regression Script. Put in the data preprocessing code.

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression.py
@@ -0,0 +1,27 @@
+# Multiple Linear Regression. 
+# Assumptions: 
+# 1. Linearity
+# 2. Homoscedasticity
+# 3. Mutlivariate Normality
+# Independence of Errors
+# Lack of multicollinearity
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+# Splitting the dataset into the trating set and the Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 1/3, random_state = 0)
+
+## Feature Scaling
+#from sklearn.preprocessing import StandardScaler
+#sc_X = StandardScaler()
+#X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
