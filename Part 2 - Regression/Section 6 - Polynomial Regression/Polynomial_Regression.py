a58cab750114cd1d9ff662b44ffe07df6c93e91e
BOB <schlagadam@gmail.com>
2020-04-04 16:40:46 -0500

    Fitting and Graphs added to polynomial predictions

--- Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression.py
@@ -18,8 +18,25 @@ lin_reg.fit(X,y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 6)
 X_poly = poly_reg.fit_transform(X)
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly,y)
+
+# Visulaising the Linear Regression Results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X),color = 'blue')
+plt.title("Truth or Bluff(Linear Regressoion")
+plt.ylabel("Salary")
+plt.xlabel("Position Level")
+plt.show()
+
+# Visualising the Polynomial Regression Results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)),color = 'blue')
+plt.title("Truth or Bluff(Polynomial Regression")
+plt.ylabel("Salary")
+plt.xlabel("Position Level")
+plt.show()
+
 
