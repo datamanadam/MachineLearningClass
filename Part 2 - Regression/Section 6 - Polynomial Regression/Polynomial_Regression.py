65aab21b7025fe30f9c194a0a61712f6612a7bef
BOB <schlagadam@gmail.com>
2020-04-12 12:41:58 -0500

    Regression Template Created

--- Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression.py
@@ -32,11 +32,25 @@ plt.xlabel("Position Level")
 plt.show()
 
 # Visualising the Polynomial Regression Results
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)),color = 'blue')
+plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)),color = 'blue')
 plt.title("Truth or Bluff(Polynomial Regression")
 plt.ylabel("Salary")
 plt.xlabel("Position Level")
 plt.show()
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with a Polynomial Regression
+lin_reg2.predict(poly_reg.fit_transform(6.5))
+
+
+
+
+
+
 
 
