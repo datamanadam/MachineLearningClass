6a2713bda9efd891adccc0ea43067d1b0d45f287
BOB <schlagadam@gmail.com>
2020-04-23 19:28:52 -0500

    Created Decision Tree Regression.

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR.py
@@ -18,22 +18,27 @@ y = dataset.iloc[:,-1].values
 #from sklearn.cross_validation import train_test_split
 #X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
 
-## Feature Scaling
-#from sklearn.preprocessing import StandardScaler
-#sc_X = StandardScaler()
-#X_train = sc_X.fit_transform(X_train)
-#X_test = sc_X.transform(X_test)
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
 
 # Fitting SVR Model to the dataset
-
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X,y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+# Get the unscalled prediction
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
 # Visualising the  SVR Results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X),color = 'blue')
-plt.title("Truth or Bluff(Regression Model")
+plt.title("Truth or Bluff(SVR Model")
 plt.ylabel("Salary")
 plt.xlabel("Position Level")
 plt.show()
